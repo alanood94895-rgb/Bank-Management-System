4cee1a77b85a642108e3935dff37c56c1218ec29
CompanyProfile <alanood94895@gmail.com>
2026-06-11 09:06:38 +0400

    Create customer repository interface

--- src/main/java/com/example/BMS/Repositories/CustomerRepositories.java
+++ src/main/java/com/example/BMS/Repositories/CustomerRepositories.java
@@ -1,5 +1,17 @@
 package com.example.BMS.Repositories;
 
-public class CustomerRepositories {
+import com.example.BMS.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepositories extends JpaRepository<Customer,Integer> {
+
+        @Query("select c from Customer c where c.id=:id ")
+        Customer getCustomerByID(@Param("id")Integer id);
+        @Query("select c from Customer c where c.customerName=:name")
+        Customer getCustomerByName(@Param("customerName") String customerName);
 
 }
