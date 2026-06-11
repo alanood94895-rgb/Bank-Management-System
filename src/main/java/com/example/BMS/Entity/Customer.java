388c01882e80ae23f214b93b3b5ac479e47515c1
CompanyProfile <alanood94895@gmail.com>
2026-06-11 08:57:43 +0400

    Add the cutomer information

--- src/main/java/com/example/BMS/Entity/Customer.java
+++ src/main/java/com/example/BMS/Entity/Customer.java
@@ -1,5 +1,29 @@
 package com.example.BMS.Entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+@Data
+@Entity
+@Table
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer customerId;
+
+    @NotBlank(message = "Customer name cannot be empty")
+    private String customerName;
+    @Column(unique = true)
+    @NotBlank(message = "Account number cannot be empty")
+    private String accountNumber;
+    @PositiveOrZero(message = "Balance cannot be negative")
+    private Double balance;
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+    private String phoneNumber;
 
 }
